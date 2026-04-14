2c5b42e026bf2acc8cb0f8d4ad0b819dfe45ddef
nabin47 <49593276+nabin47@users.noreply.github.com>
2026-04-14 18:36:17 +0000

    Deploying to gh-pages from @ nabin47/nabin47.github.io@77f25b153b8a2b74e2451729badc6e1de372ac1c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -172,14 +172,14 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%6A%61%68%6D%65%64.%63%73%65@%69%75%62%61%74.%65%64%75", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://www.linkedin.com/in/ahmednabin", "_blank");
         },
       },{
         id: 'social-rss',
@@ -193,14 +193,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=Rnh7FmQAAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
